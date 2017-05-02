e71f60921ef32df4556efdb16c77c4f56a162ce9
Thomas Hayes <thomashayes7889@gmail.com>
2017-05-01 21:30:46 -0500

    added quotes into correct div tags

--- resources/views/about/aboutUs.blade.php
+++ resources/views/about/aboutUs.blade.php
@@ -7,17 +7,24 @@
 
 	<div class="col-sm-4">
 		<img src="/img/aaron.png" height="225" width="300">
+		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
+
+		<p class="text-right">— Justin Dewie Wilson</p>
+
+		<hr>
 	</div>	
 
 	<div class="col-sm-4">
 		<img src="/img/allison.png" height="225" width="300">
+		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
+
+		<p class="text-right">— Justin Dewie Wilson</p>
+
+		<hr>
 	</div>
 
 	<div class="col-sm-4">
 		<img src="/img/brandon.png" height="225" width="300">
-	</div>
-
-	<div class="col-sm-4">
 		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
 
 		<p class="text-right">— Justin Dewie Wilson</p>
@@ -25,57 +32,35 @@
 		<hr>
 	</div>
 
-	<div class="col-sm-4">
-		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
 
-		<p class="text-right">— Justin Dewie Wilson</p>
-
-		<hr>
-	</div>
-
-	<div class="col-sm-4">
-		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
-
-		<p class="text-right">— Justin Dewie Wilson</p>
-
-		<hr>
-	</div>
 
 	<div class="col-sm-4">
 		<img src="/img/dewie.png" height="225" width="300">
+		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
+
+		<p class="text-right">— Justin Dewie Wilson</p>
+
+		<hr>
 	</div>	
 
 	<div class="col-sm-4">
 		<img src="/img/dude1.png" height="225" width="300">
+		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
+
+		<p class="text-right">— Justin Dewie Wilson</p>
+
+		<hr>
 	</div>	
 
 	<div class="col-sm-4">
 		<img src="/img/dude2.png" height="225" width="300">	
+		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
+
+		<p class="text-right">— Justin Dewie Wilson</p>
+
+		<hr>
 	</div>	
 
-	<div class="col-sm-4">
-		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
-
-		<p class="text-right">— Justin Dewie Wilson</p>
-
-		<hr>
-	</div>
-
-	<div class="col-sm-4">
-		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
-
-		<p class="text-right">— Justin Dewie Wilson</p>
-
-		<hr>
-	</div>
-
-	<div class="col-sm-4">
-		<p>“We here at [BRACKET STUDIO] have joined together in an effort to bring the general public videos and other products that will bring humor and delight to the lives of those who need it... or want it... basically anyone who uses the Internet!”</p>
-
-		<p class="text-right">— Justin Dewie Wilson</p>
-
-		<hr>
-	</div>
 
 </div>
 
