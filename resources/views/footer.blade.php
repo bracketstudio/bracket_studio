b77ef06a0fbe0aef87a54abd898bcb146b425b86
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-05-01 20:05:42 -0500

    created footer for pages. Added css folder

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,17 @@
+	<footer class="footer-basic-centered">
+
+			<p class="footer-company-motto">We don't give a fuck. -Dewie</p>
+
+			<p class="footer-links">
+				<a href="/about">About</a>
+				·
+				<a href="/careers">Careers</a>
+				·
+				<a href="/faq">FAQ</a>
+				·
+				<a href="/contact">Contact</a>
+			</p>
+
+			<p class="footer-company-name">Bracket Studio &copy; 2017</p>
+
+		</footer>
