fb1e1e6093bf2910d1d290477778421b1522cea3
Matt Valdez <mattvaldez01@gmail.com>
2017-05-03 22:50:18 -0500

    added simple 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container" style="text-align: center;
+            display: table-cell;
+            vertical-align: middle;">
+        <div class="content" style="text-align: center;
+            display: inline-block;">
+            <div class="title" style="margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+            font-size: 96px;">Sorry, Page Not Found</div>
+        </div>
+    </div>
+@stop
