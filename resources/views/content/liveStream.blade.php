dba0813ec0e95a7afa979742b85b25709883fc96
Joshua-Sifuentes1 <joshua.sifuentes1@gmail.com>
2017-05-05 09:53:00 -0500

    Changed twith iframe to video only no chat.

--- resources/views/content/liveStream.blade.php
+++ resources/views/content/liveStream.blade.php
@@ -5,12 +5,11 @@
 <h1>Twitch Live Stream</h1>
 <div class="video-container">
 	<iframe
-	    src="https://player.twitch.tv/?volume=0.5&channel=bracketstudio"
-	    frameborder="0"
+	    src="http://player.twitch.tv/?channel=bracketstudio"
+	    frameborder="true"
 	    scrolling="no"
 	    allowfullscreen="true">
 	</iframe>
-	
 </div>
 
 @stop
