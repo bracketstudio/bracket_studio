765d5c1218d6f7719cf8354d8a33a2dd5ec34979
Thomas Hayes <thomashayes7889@gmail.com>
2017-05-05 12:23:59 -0500

    Embedded youtube playlist. Seeing if this will display the most recent uploads whenever they upload a new video.

--- resources/views/content/videos.blade.php
+++ resources/views/content/videos.blade.php
@@ -6,10 +6,10 @@
 	<h1 class="text-center">Here we have our 100 most recently posted videos!</h1>
 	<div class="video-container">
 		<iframe 
-		src="https://www.youtube.com/embed/1R5d3vp0SY8?list=UU4jIPpJtuejO8NZGTM3xKjA" 
-		frameborder="0" 
-		allowfullscreen="true">
-		</iframe>
+			src="https://www.youtube.com/embed/videoseries?list=UU4jIPpJtuejO8NZGTM3xKjA" 
+			frameborder="0" 
+			allowfullscreen="true">		
+			</iframe>
 	</div>
 	
 	<article class="col-md-4">
