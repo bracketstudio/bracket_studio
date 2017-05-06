2596349a488e7b53d16ca5c72fd950af97ae5e8a
Matt Valdez <mattvaldez01@gmail.com>
2017-05-05 19:33:44 -0500

    styled game playlist links

--- resources/views/content/videos.blade.php
+++ resources/views/content/videos.blade.php
@@ -11,38 +11,31 @@
 			allowfullscreen="true">		
 			</iframe>
 	</div>
+	<div class="playlists text-center">
+		<h3>Other Playlists</h3>
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=X_IhLoDjbFA&list=PLnKHfRzQm6Y8KcuFU4_8Xj6gpFhk5ckW7" target="_blank"><h1>Gang Beasts</h1></a>
+		</article>
 	
-	<article class="col-md-4">
-		<h5>Gang Beasts Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=X_IhLoDjbFA&list=PLnKHfRzQm6Y8KcuFU4_8Xj6gpFhk5ckW7" target="_blank"><h1>Gang Beasts</h1></a>
-	</article>
-
-	<article class="col-md-4">
-		<h5>Overwatch Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=W9RFN6pFn1o&list=PLnKHfRzQm6Y_sQ4GfzYwBJqdQAF_WvdQZ" target="_blank"><h1>Overwatch</h1></a>
-	</article>
-
-	<article class="col-md-4">
-		<h5>Minecraft Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=bsX-d9At4C0&list=PLnKHfRzQm6Y-7R4UPRSHyd-5_nNPPINFW" target="_blank"><h1>Minecraft</h1></a>
-	</article>
-
-	<article class="col-md-4">
-		<h5>Resident Evil 7: Biohazard Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=LCvXBFxPAhw&list=PLnKHfRzQm6Y9XtNOD0le9GU6SfFS8FAkx" target="_blank"><h1>Resident Evil 7: Biohazard</h1></a>
-	</article>
-
-	<article class="col-md-4">
-		<h5>Super Smashed Bros. Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=Hp6kxdnwVAg&list=PLnKHfRzQm6Y-tlqnLXs2yjhqAp8TZ22S6" target="_blank"><h1>Super Smashed Bros.</h1></a>
-	</article>
-
-	<article class="col-md-4">
-		<h5>Rockband Playlist</h5>
-		<a href="https://www.youtube.com/watch?v=_JaEyueNjSY&list=PLnKHfRzQm6Y-CWIkHWdAyScKJdIWadg2x" target="_blank"><h1>Rockband</h1></a>
-	</article>	
-
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=W9RFN6pFn1o&list=PLnKHfRzQm6Y_sQ4GfzYwBJqdQAF_WvdQZ" target="_blank"><h1>Overwatch</h1></a>
+		</article>
 	
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=bsX-d9At4C0&list=PLnKHfRzQm6Y-7R4UPRSHyd-5_nNPPINFW" target="_blank"><h1>Minecraft</h1></a>
+		</article>
 	
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=LCvXBFxPAhw&list=PLnKHfRzQm6Y9XtNOD0le9GU6SfFS8FAkx" target="_blank"><h1>Resident Evil 7: Biohazard</h1></a>
+		</article>
+	
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=Hp6kxdnwVAg&list=PLnKHfRzQm6Y-tlqnLXs2yjhqAp8TZ22S6" target="_blank"><h1>Super Smashed Bros.</h1></a>
+		</article>
+	
+		<article class="col-md-4">
+			<a href="https://www.youtube.com/watch?v=_JaEyueNjSY&list=PLnKHfRzQm6Y-CWIkHWdAyScKJdIWadg2x" target="_blank"><h1>Rockband</h1></a>
+		</article>	
+	</div>
 </div>
 @stop
